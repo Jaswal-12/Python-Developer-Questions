a185390eb16117f944859326ccc49579ca0cc0f9
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-01-14 11:55:11 +0530

    Add vowel checking program in Python

---
+++ 01_Python_Basics/WAP to check if a single character is a vowel or not.py
@@ -0,0 +1,7 @@
+x = input("Enter a character: ")
+
+if (x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U' or
+    x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u'):
+    print("Yes it is a vowel")
+else:
+    print("No it is not a vowel")
