2c47c3b719201bd204f96b4f72dad956f3239114
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-01-14 12:07:10 +0530

    Add program to check if string starts with a vowel

---
+++ 01_Python_Basics/Write a Python program to check whether a string starts with vowel or not.py
@@ -0,0 +1,9 @@
+a=str(input("Enter text: "))
+x=a[0]
+
+
+if (x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U' or
+    x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u'):
+    print("Yes it is a vowel")
+else:
+    print("No it is not a vowel")
