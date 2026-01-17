8457494069766b0da5441ada0a38d21d16f30bb7
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-01-17 09:40:25 +0530

    Create factorial_of_number.py

---
+++ 02_for_loop/factorial_of_number.py
@@ -0,0 +1,13 @@
+# by using inbuilt  function
+x = int(input("Enter a number: "))
+print(x.fact())
+
+# -------------------------------------------
+
+
+x = int(input("Enter a number: "))
+p=1
+
+for i in range(1,x+1):
+    p=p*i
+print(p)
