69807de0210360e404f91274b91d3e6114a2dd09
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-01-20 10:49:25 +0530

    Update Even_number_list.py

--- 03_Functions/Even_number_list.py
+++ 03_Functions/Even_number_list.py
@@ -1,3 +1,10 @@
-
+# first approach
 even_numbers = list(range(4, 31, 2))
 print(even_numbers)
+
+# second approach
+l=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+for i in range(len(l)):
+    if(l[i]%2==0):
+        print(l[i])
